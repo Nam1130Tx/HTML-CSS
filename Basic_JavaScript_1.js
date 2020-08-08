9c50fe7fb5320433f967861140c3c4389e2dc810
Nam1130Tx <69258625+Nam1130Tx@users.noreply.github.com>
2020-08-08 12:42:39 -0500

    Add HTML/JavaScript
    Day 2 tasks

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Alert Method");
+function My_First_Function() {
+	var string = "Kiss me, I'm Irish!";
+	var result = string.fontcolor("green");
+	document.getElementById("Irish").innerHTML =
+	result;
+}
